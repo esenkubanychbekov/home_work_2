395bf03f97e33553208bcdac1b13e8617971103a
esenkubanychbekov <esen.kubanichbekov@gmail.com>
2019-02-20 22:24:37 +0600

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
      String git_test = "Готовим всё для проверки гита";
-     //запускаем команду git init
+     System.out.println(git_test);
         String massage = "Желаеете ли вы создать коммит ?";
+        System.out.println(massage);
         Scanner answer1 = new Scanner(System.in);
         String answer_1 = answer1.nextLine();
-        Scanner answer2 = new Scanner(System.in);
-        String answer_2 = answer2.nextLine();
-        if( answer_1 = "Yes")
-        {
+        if( answer_1 == "Да") {
             System.out.println("Создается первый коммит...");
         }
-        if (answer_2 == "No")
-        {
+        else if (answer_1 == "Нет") {
             System.out.println("Создание первого коммита отменено...");
         }
-        else
-        System.out.println("Неверная команда, повторите попытку");
+        else {
+            System.out.println("Неверная команда, повторите попытку");
+        }
 
 
 
